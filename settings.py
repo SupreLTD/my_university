c59f9a0318995215a3b2a9e23d500e1b2273576d
ivan <supreltd@gmail.com>
2023-05-05 09:54:48 +0300

    test logic

--- settings.py
+++ settings.py
@@ -9,5 +9,5 @@ REAL_DATABASE_URL = env.str(
 
 TEST_DATABASE_URL = env.str(
     'TEST_DATABASE_URL',
-    default='postgresql+asyncpg://postgres:postgres@0.0.0.0:5433/postgres_test'
+    default='postgresql+asyncpg://postgres_test:postgres_test@0.0.0.0:5433/postgres_test'
 )
