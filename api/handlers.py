58409d835de52760d51a2af963485be9ebfe0f72
Vanya <balanaaarrrr@gmail.com>
2023-07-31 21:07:32 +0300

    some

--- api/handlers.py
+++ api/handlers.py
@@ -29,3 +29,6 @@ async def _create_new_user(body: UserCreate, db) -> ShowUser:
 @user_router.post('/', response_model=ShowUser)
 async def create_user(body: UserCreate, db: AsyncSession = Depends(get_db)):
     return await _create_new_user(body, db)
+
+
+a = {"sdvkdjvnsdajnsda"}
